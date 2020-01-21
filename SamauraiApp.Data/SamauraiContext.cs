3c30157a83ef7cfa9b9ca4a22bdb79daac93513e
Thanzeel <tnlthanzeel@gmail.com>
2020-01-21 06:41:16 +0530

    Add project files.

---
+++ SamauraiApp.Data/SamauraiContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamauraiApp.Domain;
+
+namespace SamauraiApp.Data
+{
+    public class SamauraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        private const string _dbConnectionString = "Server=DESKTOP-PHMAO3P\\SQLSERVER17;database=efcore31samrui;Trusted_connection=true;pooling=true;MultipleActiveResultSets=True";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_dbConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.BattleId, s.SamuraiId });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ SamauraiApp.Domain/Battle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamauraiApp.Domain
+{
+    public class Battle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDare { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<SamuraiBattle> SamuraiBattles { get; set; }
+    }
+}
